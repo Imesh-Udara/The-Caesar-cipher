a49619f697709170c0f4d3497b340fe17c2f11cb
Imesh <2019cs171@stu.ucsc.lk>
2021-08-09 09:53:18 +0530

    The Caesar cipher

---
+++ caesar/src/seasar.scala
@@ -0,0 +1,35 @@
+object seasar {
+  def main(args: Array[String]): Unit = {
+
+
+      val encryption=(c:Char,key:Int,alphabet:String)=>{
+        alphabet((alphabet.indexOf(c)+key)%alphabet.size)
+      }
+
+      val decryption=(c:Char,key:Int,alphabet:String)=>{
+        if(alphabet.indexOf(c)<key)
+        {
+          alphabet(alphabet.indexOf(c)-key+alphabet.length())
+        }
+        else
+        {
+          alphabet((alphabet.indexOf(c)-key)%alphabet.size)
+        }
+      }
+
+      val cipher=(f:(Char,Int,String)=>Char,text:String,key:Int,alphabet:String)=>text.map(f(_,key,alphabet))
+
+
+      val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+      var text="The Caesar Cipher is a very simple \nencryption technique in which one replaces each \nletter in the plaintext for another letter that is a fxed \nnumber of positions down the alphabet."
+
+      val ct=cipher(encryption,text,5,alphabet)
+      val pt=cipher(decryption,ct,5,alphabet)
+
+      println("Text     : "+text)
+      println("Encrypted Text: "+ct)
+      println("Decrypted Text: "+pt)
+
+    }
+  //}
+}
